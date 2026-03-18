aa24c95eff3b5e8f8ba98bd6c0c89f5520700ce9
Md salim islam <salimislam0036@gmail.com>
2026-03-18 16:36:40 +0600

    fix(header): redirect unauthenticated users to login on cart and wishlist click

--- src/context/useRequireAuth/useRequireAuth.jsx
+++ src/context/useRequireAuth/useRequireAuth.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../AuthContext";
+// import { useAuth } from "../context/AuthContext";
 
 const useRequireAuth = () => {
   const { isLoggedIn } = useAuth();
