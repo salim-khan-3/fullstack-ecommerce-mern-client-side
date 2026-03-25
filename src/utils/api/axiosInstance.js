dfc64ea9caf584724337fcc1fb7e157e1c2d3f03
Salim <salimislam0036@gmail.com>
2026-03-24 18:38:02 -0700

    feat: add axios instance with fullstack ecommerce server base URL

--- src/utils/api/axiosInstance.js
+++ src/utils/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://fulls-stack-ecommerce-mern-server-s.vercel.app/api', 
+  baseURL: 'https://fullstack-ecommerce-server-nine.vercel.app/api', 
   timeout: 10000, //
   headers: {
     'Content-Type': 'application/json',
